b7fa0dd8b165e56cbb9689af01169bbe3d9fe223
AshtaPalak <ashtapalak@gmail.com>
2026-07-24 00:46:23 +0530

    chore(main): update startup greeting messages

--- Ficometer-1/src/main/java/com/Ficometer1Application.java
+++ Ficometer-1/src/main/java/com/Ficometer1Application.java
@@ -8,7 +8,9 @@ public class Ficometer1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ficometer1Application.class, args);
-		System.out.println("Hello World!");
+		System.out.println("Hello !");
+		System.out.println("Hello Palak!");
+
 	}
 
 }
